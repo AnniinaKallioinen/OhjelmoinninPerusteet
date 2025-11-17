11e69ef85ce14192bbdd8bb123d596c0fb3b74e5
Anniina Kallioinen <anniina.kallioinen@centria.fi>
2025-11-17 14:27:42 +0200

    muutoksia

--- Viikko2/lue_varaukset.py
+++ Viikko2/lue_varaukset.py
@@ -24,11 +24,24 @@ def main():
     with open(varaukset, "r", encoding="utf-8") as f:
         varaus = f.read().strip()
 
-    # Tulostetaan varaus konsoliin
+    from datetime import datetime
+    paiva = datetime.strptime(varaus[2], "%Y-%m-%d").date()
+    suomalainenPaiva = paiva.strftime("%d.%m.%Y")
+    aika = datetime.strptime(varaus[3], "%H:%M").time()
+    suomalainenAika = aika.strftime("%H.%M")
+
     print(varaus.split('|'))
-    varausId = varaus.split('|')[0]
-    print(varausId)
-    print(type(varausId))
+    varausnumero = int(varaus[0])
+    varaaja = str(varaus[1])
+    päivämäärä = datetime.date(varaus[2])
+    aloitusaika = datetime.time(varaus[3])
+    tuntimäärä = int(varaus[4])
+    tuntihinta = float(varaus[5])
+    maksettu = bool(varaus[6])
+    varauskohde = str(varaus[7])
+    puhelin = int(varaus[8])
+    sähköposti = str(varaus[9])
+
 
 if __name__ == "__main__":
     main()
